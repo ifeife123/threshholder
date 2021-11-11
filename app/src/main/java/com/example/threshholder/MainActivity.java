b103575d6161a08d7c6850a245f85fb92b32fe71
ifeife123 <30591068+ifeife123@users.noreply.github.com>
2021-11-11 08:59:02 -0500

    emore descriptive variable names

--- app/src/main/java/com/example/threshholder/MainActivity.java
+++ app/src/main/java/com/example/threshholder/MainActivity.java
@@ -10,7 +10,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.threshholder.MESSAGE";
+    Double time_exposure, N, Co, V, P2, epsilon, lambda_a, Qb, pm, cq, change_in_c2;
 
+    EditText time_exposure_input,N_input, Co_input, V_input, P2_input, epsilon_input, lambda_a_input, Qb_input, pm_input, cq_input, change_in_c2_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,22 +20,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayThreshold(View view){
-        EditText editText = (EditText) findViewById(R.id.editTextTimeExposure);
-        Double time_exposure = Double.valueOf(editText.getText().toString());
-        editText = findViewById(R.id.editTextNpeople);
-        Double N = Double.valueOf(editText.getText().toString());
+        time_exposure_input = findViewById(R.id.editTextTimeExposure);
+        time_exposure = Double.valueOf(time_exposure_input.getText().toString());
+        N_input = findViewById(R.id.editTextNpeople);
+        N = Double.valueOf(N_input.getText().toString());
 
-        editText = findViewById(R.id.editTextCo);
-        Double Co = Double.valueOf(editText.getText().toString());
+        Co_input = findViewById(R.id.editTextCo);
+        Co = Double.valueOf(Co_input.getText().toString());
 
-        editText = findViewById(R.id.editTextV);
-        Double V = Double.valueOf(editText.getText().toString());
+        V_input = findViewById(R.id.editTextV);
+        V = Double.valueOf(V_input.getText().toString());
 
-        editText = findViewById(R.id.editTextPt);
-        Double P2 = Double.valueOf(editText.getText().toString());
+        P2_input = findViewById(R.id.editTextPt);
+        P2 = Double.valueOf(P2_input.getText().toString());
 
-
-        double change_in_c2 = 0;
         change_in_c2 = time_exposure*N*P2/V;
 
 
@@ -43,42 +43,42 @@ public class MainActivity extends AppCompatActivity {
         }else{
             message = "below threshold";
         }
-        TextView tv = (TextView)findViewById(R.id.textViewOutput);
+        TextView tv = findViewById(R.id.textViewOutput);
         tv.setText(message);
 //        intent.putExtra(EXTRA_MESSAGE, message);
 //        startActivity(intent);
     }
     public void displayTMax(View view){
-        EditText editText = (EditText) findViewById(R.id.editTextTimeExposure);
-        Double time_exposure = Double.valueOf(editText.getText().toString());
+
+        time_exposure_input = findViewById(R.id.editTextTimeExposure);
+        time_exposure = Double.valueOf(time_exposure_input.getText().toString());
+
+        N_input = findViewById(R.id.editTextNpeople);
+        N = Double.valueOf(N_input.getText().toString());
+
+        epsilon_input = findViewById(R.id.editTextPt);
+        epsilon = Double.valueOf(epsilon_input.getText().toString());
+
+        lambda_a_input = findViewById(R.id.editTextACH);
+        lambda_a = Double.valueOf(lambda_a_input.getText().toString());
+
+        Qb_input = findViewById(R.id.editTextQb);
+        Qb = Double.valueOf(Qb_input.getText().toString());
+
+        pm_input = findViewById(R.id.editTextPm);
+        pm = Double.valueOf(pm_input.getText().toString());
+
+        cq_input = findViewById(R.id.editTextCq);
+        cq = Double.valueOf(cq_input.getText().toString());
 
 
-        editText = findViewById(R.id.editTextNpeople);
-        Double N = Double.valueOf(editText.getText().toString());
 
-        editText = findViewById(R.id.editTextPt);
-        Double epsilon = Double.valueOf(editText.getText().toString());
-
-        editText = findViewById(R.id.editTextACH);
-        Double lambda_a = Double.valueOf(editText.getText().toString());
-
-        editText = findViewById(R.id.editTextQb);
-        Double Qb = Double.valueOf(editText.getText().toString());
-
-        editText = findViewById(R.id.editTextPm);
-        Double pm = Double.valueOf(editText.getText().toString());
-
-        editText = findViewById(R.id.editTextCq);
-        Double cq = Double.valueOf(editText.getText().toString());
-
-
-
-        double T_max = 0;
+        double T_max;
         T_max = epsilon*lambda_a/(N*Math.pow(Qb,2)*Math.pow(pm,2)*cq);
 
         String message;
-        message = String.valueOf(T_max) + " is the T_max";
-        TextView tv = (TextView)findViewById(R.id.textViewOutput);
+        message = T_max + " is the T_max";
+        TextView tv = findViewById(R.id.textViewOutput);
         tv.setText(message);
 //        intent.putExtra(EXTRA_MESSAGE, message);
 //        startActivity(intent);
