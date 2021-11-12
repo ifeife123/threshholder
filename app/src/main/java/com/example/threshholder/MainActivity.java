4cbacdc9d067a40708fefb28745f6331eaef64bf
ifeife123 <30591068+ifeife123@users.noreply.github.com>
2021-11-12 14:27:22 -0500

    Remove old stuff from Co_ thresholding task. Extra variables included p2, time exposure and p2

--- app/src/main/java/com/example/threshholder/MainActivity.java
+++ app/src/main/java/com/example/threshholder/MainActivity.java
@@ -10,44 +10,47 @@ import android.widget.TextView;
 import static java.lang.Math.*;
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.threshholder.MESSAGE";
-    Double time_exposure, N, Co, V, P2, epsilon, lambda_a, Qb, pm, cq, change_in_c2;
+    Double time_exposure, N,  V, epsilon, lambda_a, Qb, pm, cq, sr;
+//    Double Co,P2, change_in_c2;
 
-    EditText time_exposure_input,N_input, Co_input, V_input, P2_input, epsilon_input, lambda_a_input, Qb_input, pm_input, cq_input, change_in_c2_input;
+    EditText time_exposure_input,N_input, V_input, epsilon_input, lambda_a_input, Qb_input, pm_input, cq_input, sr_input;
+//    EditText Co_input,P2_input, Change_in_c2_input;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void displayThreshold(View view){
-        time_exposure_input = findViewById(R.id.editTextTimeExposure);
-        time_exposure = Double.valueOf(time_exposure_input.getText().toString());
-        N_input = findViewById(R.id.editTextNpeople);
-        N = Double.valueOf(N_input.getText().toString());
-
-        Co_input = findViewById(R.id.editTextCo);
-        Co = Double.valueOf(Co_input.getText().toString());
-
-        V_input = findViewById(R.id.editTextV);
-        V = Double.valueOf(V_input.getText().toString());
-
-        P2_input = findViewById(R.id.editTextPt);
-        P2 = Double.valueOf(P2_input.getText().toString());
-
-        change_in_c2 = time_exposure*N*P2/V;
-
-
-        String message;
-        if(change_in_c2 > Co){
-            message = "above threshold";
-        }else{
-            message = "below threshold";
-        }
-        TextView tv = findViewById(R.id.textViewOutput);
-        tv.setText(message);
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-    }
+//    public void displayThreshold(View view){
+//        time_exposure_input = findViewById(R.id.editTextTimeExposure);
+//        time_exposure = Double.valueOf(time_exposure_input.getText().toString());
+//        N_input = findViewById(R.id.editTextNpeople);
+//        N = Double.valueOf(N_input.getText().toString());
+//
+//        Co_input = findViewById(R.id.editTextCo);
+//        Co = Double.valueOf(Co_input.getText().toString());
+//
+//        V_input = findViewById(R.id.editTextV);
+//        V = Double.valueOf(V_input.getText().toString());
+//
+//        P2_input = findViewById(R.id.editTextPt);
+//        P2 = Double.valueOf(P2_input.getText().toString());
+//
+//        change_in_c2 = time_exposure*N*P2/V;
+//
+//
+//        String message;
+//        if(change_in_c2 > Co){
+//            message = "above threshold";
+//        }else{
+//            message = "below threshold";
+//        }
+//        TextView tv = findViewById(R.id.textViewOutput);
+//        tv.setText(message);
+////        intent.putExtra(EXTRA_MESSAGE, message);
+////        startActivity(intent);
+//    }
 
     static double Round_off(double N, double n)
     {
@@ -82,13 +85,10 @@ public class MainActivity extends AppCompatActivity {
     }
     public void displayTMax(View view){
 
-        time_exposure_input = findViewById(R.id.editTextTimeExposure);
-        time_exposure = Double.valueOf(time_exposure_input.getText().toString());
-
         N_input = findViewById(R.id.editTextNpeople);
         N = Double.valueOf(N_input.getText().toString());
 
-        epsilon_input = findViewById(R.id.editTextPt);
+        epsilon_input = findViewById(R.id.editTextRiskTolerance);
         epsilon = Double.valueOf(epsilon_input.getText().toString());
 
         lambda_a_input = findViewById(R.id.editTextACH);
@@ -103,13 +103,18 @@ public class MainActivity extends AppCompatActivity {
         cq_input = findViewById(R.id.editTextCq);
         cq = Double.valueOf(cq_input.getText().toString());
 
+        V_input = findViewById(R.id.editTextV);
+        V = Double.valueOf(V_input.getText().toString());
 
+        sr_input = findViewById(R.id.editTextSr);
+        sr = Double.valueOf(sr_input.getText().toString());
 
         double T_max;
-        T_max = epsilon*lambda_a/(N*Math.pow(Qb,2)*Math.pow(pm,2)*cq);
+        T_max = epsilon*lambda_a*V/(N*Math.pow(Qb,2)*Math.pow(pm,2)*cq*sr);
 
         String message;
-        message = Round_off(T_max, 3) + " is the T_max";
+        message = Round_off(T_max, 3) + " hours is the maximum exposure time";
+//        message = T_max+ " is the maximum exposure time";
         TextView tv = findViewById(R.id.textViewOutput);
         tv.setText(message);
 //        intent.putExtra(EXTRA_MESSAGE, message);
